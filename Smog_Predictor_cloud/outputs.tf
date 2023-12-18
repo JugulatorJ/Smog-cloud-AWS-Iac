30965c90523da77f2cac4ddbd9b87482a9382f1c
JugulatorJ <markiewicz_krzysztof87@gmail.com>
2023-12-18 22:29:19 +0100

    Modified (commented) output.tf for further modifications.

---
+++ Smog_Predictor_cloud/outputs.tf
@@ -0,0 +1,35 @@
+# output "VPC id" {
+#   description = "VPC id"
+#   value       = aws_vpc.smog_vpc.id
+# }
+
+# output "VPC arn" {
+#   description = "VPC arn"
+#   value       = aws_vpc.smog_vpc.arn
+# }
+
+# output "Public subnet 1 id" {
+#     description = "Id of public subnet 1"
+#     value = aws_subnet.public_web1.id
+# }
+
+# output "Public subnet 2 id" {
+#     description = "Id of public subnet 2"
+#     value = aws_subnet.public_web2.id
+# }
+
+
+# output "Private subnet 1 id" {
+#     description = "ID of private subnet 1"
+#     value = aws_subnet.private_storage_compute1
+# }
+
+# output "Private subnet 2 id" {
+#     description = "ID of private subnet 2"
+#     value = aws_subnet.private_storage_compute2
+# }
+
+# output "Internet gateway VPC id" {
+#     description = "Internet gateway VPC id"
+#     value       = aws_internet_gateway.smog_IGW.vpc_id
+# }
